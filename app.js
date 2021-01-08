454a79f0fd1cae446d8447a54674362213aeecfe
B1ner0 <degenaarp@gmail.com>
2021-01-08 15:02:24 +0200

    Added more

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser())
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.send("THIS IS THE HOMEPAGE");
 })
 
 app.post('/test', (req, res) => {
-    console.log(req.body.email);
+    console.log(req.body);
 
     res.send("OK");
+})
+
+app.listen(PORT, () => {
+    console.log("Server is listening on PORT " + PORT);
 })
